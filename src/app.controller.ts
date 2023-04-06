415c020c8e011aeedbca10110b63c5cd58e24d99
JunhaDex <dk.junha.dev@gmail.com>
2023-04-06 19:06:30 +0900

    add sendgrid mailer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as sgMail from '@sendgrid/mail';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/send')
+  async sendMessage(): Promise<string> {
+    const mailer = sgMail;
+    mailer.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: '',
+      from: '',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    await mailer.send(msg).catch((e) => console.error(e));
+    return 'ok';
   }
 }
