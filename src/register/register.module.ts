26cfe5ebdc959e0634ed38831b7aa64be12974c3
JunhaDex <dk.junha.dev@gmail.com>
2023-04-18 00:45:00 +0900

    add middleware

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { RegisterService } from './register.service';
 import { RegisterController } from './register.controller';
+import { FireauthMiddleware } from '../middleware/fireauth.middleware';
 
 @Module({
   controllers: [RegisterController],
   providers: [RegisterService],
 })
-export class RegisterModule {}
+export class RegisterModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(FireauthMiddleware)
+      .forRoutes({ path: '/register', method: RequestMethod.POST });
+  }
+}
