e3f095f6dc515c4d9a4bd25869f0e74f2a01f7f3
JunhaDex <dk.junha.dev@gmail.com>
2023-06-14 23:14:34 +0900

    add email send cli

--- command/functions/email.func.ts
+++ command/functions/email.func.ts
@@ -1,8 +1,21 @@
 import * as fs from 'fs';
+import sgMail from '@sendgrid/mail';
 
 export async function sendGroupEmail() {
-  const template = fs.readFileSync('../templates/welcome.html', {
+  console.log(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const template = fs.readFileSync('./command/templates/welcome.html', {
     encoding: 'utf8',
   });
-  console.log(template);
+
+  const filled = template.replace('<%user_name%>', '');
+  const msg = {
+    to: '',
+    from: 'noreply@kysa.page',
+    subject: '참석축하메일',
+    html: filled,
+  };
+  const result = await sgMail.send(msg);
+  console.log(result[0].statusCode);
+  console.log(result[0].headers);
 }
--- command/main.ts
+++ command/main.ts
@@ -3,6 +3,9 @@ import { createAppUser } from './functions/user.func';
 import * as process from 'process';
 import { updateSheetInfo } from './functions/register.func';
 import { sendGroupEmail } from './functions/email.func';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const program = new Command();
 program
